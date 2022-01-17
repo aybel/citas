f7af7fe21134bcd68c8c9d99b46d427edf0fce40
Abel <j.abel.vera@gmail.com>
2022-01-17 15:03:31 -0600

    Se agrega autor del proyecto

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,9 +1,20 @@
 const Header = () => {
     return (
-        <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
-            Seguimiento pacientes {''}
-            <span className="text-indigo-700">Veterinaria</span>
-        </h1>
+        <>
+            <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
+                Seguimiento pacientes {''}
+                <span className="text-indigo-700">Veterinaria</span>
+            </h1>
+            <div className="font-black text-4md text-center md:w-2/3 mx-auto">
+                <span >
+                    Autor: J. Abel Vera Cruz
+                    Desarrollo trabajado en React Js, 
+                    TalwindCSS y LocalStorage
+                    Enero 2022
+                </span>
+            </div>
+
+        </>
     );
 }
 
